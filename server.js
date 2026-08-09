3e2638b071ff0630e8916f8629a335ac84c7b39d
Ryan <ryanmaulana03@guru.smk.belajar.id>
2026-08-09 19:09:38 +0700

    fix: explicit __dirname in server.js to fix cPanel 503 Passenger error

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
+const path = require('path');
+const fs = require('fs');
+
+// Menangkap error tersembunyi cPanel dan menyimpannya ke file log
+process.on('uncaughtException', (err) => {
+  fs.appendFileSync(path.join(__dirname, 'cpanel-error.log'), err.stack + '\n');
+  process.exit(1);
+});
 
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
 
-const app = next({ dev });
+// SANGAT PENTING: Passenger cPanel sering salah membaca direktori root
+const app = next({ 
+  dev,
+  dir: __dirname 
+});
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -16,4 +28,7 @@ app.prepare().then(() => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
+}).catch(err => {
+  fs.appendFileSync(path.join(__dirname, 'cpanel-error.log'), 'Prepare error: ' + err.stack + '\n');
+  process.exit(1);
 });
