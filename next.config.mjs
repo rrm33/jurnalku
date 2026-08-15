aefa0b2e70584b6b0e8f5c8f845b63e159da3ed4
Ryan <ryanmaulana03@guru.smk.belajar.id>
2026-08-15 21:13:31 +0700

    chore: ignore eslint and ts errors during build to save memory

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,15 @@ const nextConfig = {
       bodySizeLimit: '20mb',
     },
   },
+  eslint: {
+    // Abaikan error eslint saat build agar lebih cepat dan hemat memori
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Abaikan error typescript saat build
+    ignoreBuildErrors: true,
+  },
+  swcMinify: true,
 };
 
 export default nextConfig;
