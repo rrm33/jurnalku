05c5b72627e328e55ce8252cac4dfb422fd51919
Ryan <ryanmaulana03@guru.smk.belajar.id>
2026-08-15 21:17:27 +0700

    fix: switch to babel to bypass swc glibc and wasm hanging issues on cpanel

--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,7 @@ const nextConfig = {
     serverActions: {
       bodySizeLimit: '20mb',
     },
-  },
-  eslint: {
-    // Abaikan error eslint saat build agar lebih cepat dan hemat memori
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Abaikan error typescript saat build
-    ignoreBuildErrors: true,
-  },
-  swcMinify: true,
+  }
 };
 
 export default nextConfig;
