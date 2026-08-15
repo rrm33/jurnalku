bfac027d02a359411612713b178867867773b338
Ryan <ryanmaulana03@guru.smk.belajar.id>
2026-08-15 21:49:59 +0700

    fix: remove thread limits causing wasm compiler panic

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    cpus: 1,
-    workerThreads: false,
     serverActions: {
       bodySizeLimit: '20mb',
     },
